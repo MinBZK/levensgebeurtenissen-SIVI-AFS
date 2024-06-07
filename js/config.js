5b66d4b8d0d7962f30a1c1cccb0e7686ea5af545
Steven Gort <steven.gort@ictu.nl>
2024-06-07 13:59:16 +0200

    added publish actions

--- js/config.js
+++ js/config.js
@@ -20,7 +20,7 @@ let respecConfig = {
     "Bevat de standaardisatie van levensgebeurtenissen en signalen in SIVI All Finance Standaard (SIVI AFS)",
 
   // Content
-  content: { ch01: "informative", ch02: "", ch03: "", ch04: "" },
+  content: { ch01: "informative", ch02: "" },
   editors: [
     {
       name: "Steven Gort",
